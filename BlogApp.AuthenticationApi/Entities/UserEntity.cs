1fe53c84b8cfcb069dfe1910c8245931321fdb16
g0k11 <gokberks92@gmail.com>
2025-02-25 21:44:26 +0300

    created user entity

---
+++ BlogApp.AuthenticationApi/Entities/UserEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogApp.AuthenticationApi.Entities
+{
+    [Table("Users")]
+    public class UserEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Role { get; set; } = "commenter"; // Varsayılan olarak "commenter"
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ BlogApp.AuthenticationApi/Middlewares/AuthMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BlogApp.Common.DependencyInjections
+{
+    public static class JwtAuthScheme
+    {
+        public static IServiceCollection AddJWTAuthScheme(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer("Bearer", options =>
+                {
+                    var key = Encoding.UTF8.GetBytes(configuration.GetSection("Authentication:Key").Value!);
+                    string issuer = configuration.GetSection("Authentication:Issuer").Value!;
+                    string audience = configuration.GetSection("Authentication:Audience").Value!;
+
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = issuer,
+                        ValidAudience = audience,
+                        IssuerSigningKey = new SymmetricSecurityKey(key)
+                    };
+                });
+            return services;
+        }
+    }
+}
